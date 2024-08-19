b206e5b59c6e92b0651d3428fc58ecaabc33a7d5
francorivaas <francoagustinrivas@hotmail.com>
2024-08-19 12:58:57 -0300

    the player moves correctly

---
+++ TP1 IA/Assets/Scripts/Player scripts/IMove.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IMove 
+{
+    void Move(Vector3 direction);
+    
+    void Look(Vector3 direction);
+    
+    void LookTarget(Transform target);
+}
