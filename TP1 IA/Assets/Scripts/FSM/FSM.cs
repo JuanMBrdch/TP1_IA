dbbea1209c248967ef49bba75077771e52ea3f84
francorivaas <francoagustinrivas@hotmail.com>
2024-08-19 17:07:55 -0300

    fsm script

---
+++ TP1 IA/Assets/Scripts/FSM/FSM.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FSM<T>
+{
+    IState<T> current;
+
+    public Action<T, IState<T>, IState<T>> onTransition = delegate { };
+    
+    public FSM() { }
+    
+    public FSM(IState<T> init)
+    {
+        SetInitial(init);
+    }
+
+    public void SetInitial(IState<T> init)
+    {
+        current = init;
+        current.Enter();
+    }
+
+    public void OnUpdate()
+    {
+        if (current != null)
+            current.Execute();
+    }
+
+    public void OnExit()
+    {
+        if (current != null)
+            current.Exit();
+    }
+
+    public void Transition(T input)
+    {
+        IState<T> newState = current.GetTransition(input);
+        if (newState == null) return;
+        var previousState = current;
+        current.Exit();
+        current = newState;
+        current.Enter();
+        onTransition(input, current, previousState);
+    }
+
+    public IState<T> GetCurrent => current;
+}
