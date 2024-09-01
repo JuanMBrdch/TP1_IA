65c56f9b5812394b54296b003ae930f962f71a79
Reid3002 <reid3002@gmail.com>
2024-09-01 04:15:39 -0300

    Cooldown Script

---
+++ TP1 IA/Assets/Scripts/Cooldown.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cooldown
+{
+    private float _cooldownTime;
+    private float _currentTime;
+    Action OnFinishedCooldown;
+    public Cooldown(float time, Action finished = null, bool startOnCooldown = false)
+    {
+        _cooldownTime = time;
+        OnFinishedCooldown = finished;
+        if (startOnCooldown)
+        {
+            Reset();
+        }
+        else
+        {
+            _currentTime = 0;
+        }
+    }
+
+    public bool OnCoolDown()
+    {
+        if (_currentTime > 0)
+        {
+           _currentTime -= Time.deltaTime;
+            return true;
+        }        
+        
+        if (_currentTime <= 0)
+        {
+            OnFinishedCooldown();
+            return false;
+        }
+
+        return true;
+    }
+
+    public void Reset()
+    {
+        _currentTime = _cooldownTime;
+    }
+}
